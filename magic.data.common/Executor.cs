c25653fd71c85fc81ceeb8b7baf5f2b06265d634
Thomas Hansen <thomas@gaiasoul.com>
2019-10-08 09:37:16 +0300

    Bettering

---
+++ magic.data.common/Executor.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Data.Common;
+using System.Threading.Tasks;
+using magic.node;
+using magic.node.extensions;
+
+namespace magic.data.common
+{
+    /// <summary>
+    /// Helper class for creating and parametrizing an SQL command of some type.
+    /// </summary>
+    public static class Executor
+    {
+        /// <summary>
+        /// Creates a new SQL command of some type, and parametrizes it with each
+        /// child node specified in the invocation node as a key/value DB parameter -
+        /// For then to invoke the specified functor lambda callback.
+        /// </summary>
+        /// <param name="input">Node containing SQL and parameters as children.</param>
+        /// <param name="connection">Database connection.</param>
+        /// <param name="functor">Lambda function responsible for executing the command somehow.</param>
+        public static void Execute(
+            Node input,
+            DbConnection connection,
+            Action<DbCommand> functor)
+        {
+            // Making sure we dispose our command after execution.
+            using (var cmd = connection.CreateCommand())
+            {
+                // Parametrizing command.
+                PrepareCommand(cmd, input);
+
+                // Invoking lambda callback supplied by caller.
+                functor(cmd);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new SQL command of some type, and parametrizes it with each
+        /// child node specified in the invocation node as a key/value DB parameter -
+        /// For then to invoke the specified functor lambda callback.
+        /// </summary>
+        /// <param name="input">Node containing SQL and parameters as children.</param>
+        /// <param name="connection">Database connection.</param>
+        /// <param name="functor">Lambda function responsible for executing the command somehow.</param>
+        /// <returns>An awaitable task.</returns>
+        public static async Task ExecuteAsync(
+            Node input,
+            DbConnection connection,
+            Func<DbCommand, Task> functor)
+        {
+            using (var cmd = connection.CreateCommand())
+            {
+                // Parametrizing command.
+                PrepareCommand(cmd, input);
+
+                // Invoking lambda callback supplied by caller.
+                await functor(cmd);
+            }
+        }
+
+        #region [ -- Private helper methods -- ]
+
+        static void PrepareCommand(DbCommand cmd, Node input)
+        {
+            cmd.CommandText = input.GetEx<string>();
+            foreach (var idxPar in input.Children)
+            {
+                var par = cmd.CreateParameter();
+                par.ParameterName = idxPar.Name;
+                par.Value = idxPar.GetEx<object>();
+                cmd.Parameters.Add(par);
+            }
+
+            // Making sure we clean nodes before invoking lambda callback.
+            input.Value = null;
+            input.Clear();
+        }
+
+        #endregion
+    }
+}
---
+++ magic.data.common/Transaction.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Data.Common;
+using magic.signals.contracts;
+
+namespace magic.data.common
+{
+     /// <summary>
+     /// Wraps a database transaction, such that it automatically is rolled back when
+     /// instance is disposed, unless it has been previously rolled back, or committed.
+     /// </summary>
+    public class Transaction : IDisposable
+    {
+        readonly DbTransaction _transaction;
+        bool _signaled;
+
+        /// <summary>
+        /// Creates a new instance of your type.
+        /// </summary>
+        /// <param name="signaler">Signaler used to retrieve connection as stack object.</param>
+        /// <param name="stackName">Name of connection in stack.</param>
+        public Transaction(ISignaler signaler, string stackName)
+        {
+            _transaction = signaler.Peek<DbConnection>(stackName).BeginTransaction();
+        }
+
+        /// <summary>
+        /// Explicitly rolls back the transaction.
+        /// </summary>
+        public void Rollback()
+        {
+            _signaled = true;
+            _transaction.Rollback();
+        }
+
+         /// <summary>
+         /// Explicitly committing your transaction.
+         /// </summary>
+        public void Commit()
+        {
+            _signaled = true;
+            _transaction.Commit();
+        }
+
+        #region [ -- Interface implementation -- ]
+
+        /// <summary>
+        /// Disposes the instance.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!_signaled)
+                _transaction.Rollback();
+        }
+
+        #endregion
+    }
+}
