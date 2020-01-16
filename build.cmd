8558d2cfa76a9d6a75c0c2d9f5aebae93c278150
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 16:25:11 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 cd %~dp0
-dotnet build magic.data.common/magic.data.common.csproj --configuration Release
+dotnet build magic.data.common/magic.data.common.csproj --configuration Release --source https://api.nuget.org/v3/index.json
