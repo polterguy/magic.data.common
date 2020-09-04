0db94d777c21de890c9e6d9deed67b8503b3f26a
Thomas Hansen <thomas@gaiasoul.com>
2020-09-04 12:17:20 +0300

    Update DataTests.cs

--- magic.data.common.tests/DataTests.cs
+++ magic.data.common.tests/DataTests.cs
@@ -3,13 +3,10 @@
  * See the enclosed LICENSE file for details.
  */
 
-using System;
 using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 using magic.node;
 using magic.node.extensions;
-using magic.data.common;
 
 namespace magic.data.common.tests
 {
@@ -47,6 +44,34 @@ namespace magic.data.common.tests
             var result = builder.Build();
             var sql = result.Get<string>();
             Assert.Equal("select * from 'foo' limit 25", sql);
+        }
+
+        [Fact]
+        public void Update()
+        {
+            // Creating node hierarchy.
+            var node = new Node();
+            node.Add(new Node("table", "foo"));
+            var values = new Node("values");
+            values.Add(new Node("field1", "howdy"));
+            node.Add(values);
+            var where = new Node("where");
+            var and = new Node("and");
+            and.Add(new Node("field2", "value2"));
+            where.Add(and);
+            node.Add(where);
+            var builder = new SqlUpdateBuilder(node, "'");
+
+            // Extracting SQL + params, and asserting correctness.
+            var result = builder.Build();
+            var sql = result.Get<string>();
+            Assert.Equal("update 'foo' set 'field1' = @v0 where ('field2' = @0)", sql);
+            var arg1 = result.Children.First();
+            Assert.Equal("@v0", arg1.Name);
+            Assert.Equal("howdy", arg1.Get<string>());
+            var arg2 = result.Children.Skip(1).First();
+            Assert.Equal("@0", arg2.Name);
+            Assert.Equal("value2", arg2.Get<string>());
         }
 
         [Fact]
