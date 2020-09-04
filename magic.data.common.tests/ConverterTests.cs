40dd3e9541b741b5793d2ed1d4b62a2b71ae07d6
Thomas Hansen <thomas@gaiasoul.com>
2020-09-04 12:25:12 +0300

    Create ConverterTests.cs

---
+++ magic.data.common.tests/ConverterTests.cs
@@ -0,0 +1,27 @@
+/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - 2020, thomas@servergardens.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using System;
+using Xunit;
+
+namespace magic.data.common.tests
+{
+    public class ConverterTests
+    {
+        [Fact]
+        public void ConvertNull()
+        {
+            var val = Converter.GetValue(DBNull.Value);
+            Assert.Null(val);
+        }
+
+        [Fact]
+        public void ConvertNotNull()
+        {
+            var val = Converter.GetValue("howdy");
+            Assert.Equal("howdy", val);
+        }
+    }
+}
